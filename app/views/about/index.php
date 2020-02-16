567948d059788d58410481cce983eac58781e7cc
Ariq Hikari <ahidayatbia@gmail.com>
2020-02-16 16:32:11 +0700

    Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,7 @@
-<h1>About Me</h1>
-<p>Halo, nama saya <?= $data["nama"]; ?>, umur saya <?= $data["umur"]; ?> tahun, saya adalah seorang <?= $data["pekerjaan"]; ?></p>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <a href="<?= BASEURL; ?>/img/profile.jpg" alt="Ariq Hikari" data-toggle="lightbox">
+        <img src="<?= BASEURL; ?>/img/profile.jpg" alt="Ariq Hikari" class="img-thumbnail" width="200">
+    </a>
+    <p>Halo, nama saya <?= $data["nama"]; ?>, umur saya <?= $data["umur"]; ?> tahun, saya adalah seorang <?= $data["pekerjaan"]; ?></p>
+</div>
--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,3 +1,9 @@
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js"></script>
+<script src="<?= BASEURL; ?>/js/main.js"></script>
+
 </body>
 
 </html>
